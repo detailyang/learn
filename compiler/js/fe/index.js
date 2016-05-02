38f3546d9ce993bf7b40ca9f8044af3164f89e4d
detailyang <detailyang@gmail.com>
2016-05-03 00:55:07 +0800

    add evaluate thanks http://lisperator.net/pltut/eval1/

--- compiler/js/fe/index.js
+++ compiler/js/fe/index.js
@@ -2,7 +2,7 @@
 * @Author: detailyang
 * @Date:   2016-04-25 02:40:20
 * @Last Modified by:   detailyang
-* @Last Modified time: 2016-05-03 00:13:54
+* @Last Modified time: 2016-05-03 00:55:01
 */
 
 'use strict';
@@ -265,7 +265,6 @@ function parse(input) {
         return a;
     }
     function parse_call(func) {
-        console.log(func);
         return {
             type: "call",
             func: func,
@@ -515,17 +514,149 @@ function myparse(input) {
     }
 }
 
-const code = `
-fib = lambda (n) if n < 2 then n else fib(n - 1) + fib(n - 2);
-println("Hello World!");
-if a <= b {
-    print(", ");
-} else {
-    println("");
+function Environment(parent) {
+    this.vars = Object.create(parent || {});
+    this.parent = parent;
 }
-`;
-const ast = parse(new InputStream(new Input(code)));
-console.log(ast.prog);
-const myast = myparse(new InputStream(new Input(code)));
-console.log(myast.prog);
 
+Environment.prototype = {
+    extend: function () {
+        return new Environment(this);
+    },
+    lookup: function(name) {
+        var scope = this;
+        while(scope) {
+            if (Object.prototype.hasOwnProperty.call(scope.vars, name)) {
+                return scope;
+            }
+            scope = scope.parent;
+        }
+    },
+    get: function(name) {
+        if (name in this.vars) {
+            return this.vars[name];
+        }
+        throw new Error(`Undefined variable ${name}`);
+    },
+    set: function(name, value) {
+        var scope = this.lookup(name);
+        if (!scope && this.parent) {
+            throw new Error(`Undefined variable ${name}`);
+        }
+        return (scope || this).vars[name] = value;
+    },
+    def: function(name, value) {
+        return this.vars[name] = value;
+    }
+}
+
+function evaluate(exp, env) {
+    switch(exp.type) {
+        case "num":
+        case "str":
+        case "bool":
+            return exp.value;
+        case "var":
+            return env.get(exp.value);
+        case "assign":
+            if (exp.left.type != "var") {
+                throw new Error("Cannot assign to " + JSON.stringify(exp.left));
+            }
+            return env.set(exp.left.value, evaluate(exp.right, env));
+        case "binary":
+            return apply_op(exp.operator,
+                            evaluate(exp.left, env),
+                            evaluate(exp.right, env));
+        case "lambda":
+            return make_lambda(env, exp);
+        case "if":
+            var cond = evaluate(exp.cond, env);
+            if (cond !== false) {
+                return evaluate(exp.then, env);
+            }
+            return exp.else ? evaluate(exp.else, env) : false;
+        case "prog":
+            var val = false;
+            exp.prog.forEach(function (exp) {
+                val = evaluate(exp, env);
+            });
+            return val;
+        case "call":
+            var func = evaluate(exp.func, env);
+            return func.apply(null, exp.args.map((arg) => {
+                return evaluate(arg, env);
+            }));
+        default:
+            throw new Error(`I don't know how to evaluate ${exp.type}`);
+    }
+}
+
+function apply_op(op, a, b) {
+    function num(x) {
+        if (typeof x != "number") {
+            throw new Error(`Expected number but got ${x.type}`);
+        }
+        return x;
+    }
+    function div(x) {
+        if (num(x) == 0) {
+            throw new Error("Divide by zero");
+        }
+        return x;
+    }
+    switch (op) {
+      case "+"  : return num(a) + num(b);
+      case "-"  : return num(a) - num(b);
+      case "*"  : return num(a) * num(b);
+      case "/"  : return num(a) / div(b);
+      case "%"  : return num(a) % div(b);
+      case "&&" : return a !== false && b;
+      case "||" : return a !== false ? a : b;
+      case "<"  : return num(a) < num(b);
+      case ">"  : return num(a) > num(b);
+      case "<=" : return num(a) <= num(b);
+      case ">=" : return num(a) >= num(b);
+      case "==" : return a === b;
+      case "!=" : return a !== b;
+    }
+    throw new Error("Can't apply operator " + op);
+}
+
+function make_lambda(env, exp) {
+    function lambda() {
+        var names = exp.vars;
+        var scope = env.extend();
+        for (var i = 0; i < names.length; ++i) {
+            scope.def(names[i], i < arguments.length ? arguments[i] : false);
+        }
+        return evaluate(exp.body, scope);
+    }
+    return lambda;
+}
+
+
+// const code = `
+// fib = lambda (n) if n < 2 then n else fib(n - 1) + fib(n - 2);
+// println("Hello World!");
+// if a <= b {
+//     print(", ");
+// } else {
+//     println("");
+// }
+// `;
+// some test code here
+const code = "sum = lambda(x, y) x + y; print(sum(2, 3));";
+
+
+// remember, parse takes a TokenStream which takes an InputStream
+const ast = parse(new InputStream(new Input(code)));
+// create the global environment
+const globalEnv = new Environment();
+
+// define the "print" primitive function
+globalEnv.def("print", function(txt){
+  console.log(txt);
+});
+
+// run the evaluator
+evaluate(ast, globalEnv); // will print 5
