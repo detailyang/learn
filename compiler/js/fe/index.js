cf0e670ce38fd5c6a291984f4f366f6f943f7ea1
detailyang <detailyang@gmail.com>
2016-05-02 16:42:29 +0800

    know the reason why maybe_call

--- compiler/js/fe/index.js
+++ compiler/js/fe/index.js
@@ -2,7 +2,7 @@
 * @Author: detailyang
 * @Date:   2016-04-25 02:40:20
 * @Last Modified by:   detailyang
-* @Last Modified time: 2016-04-30 17:23:33
+* @Last Modified time: 2016-05-02 16:41:42
 */
 
 'use strict';
@@ -199,31 +199,240 @@ function InputStream(input) {
 }
 
 const code = `
-# this is a comment
-
 println("Hello World!");
-
-println(2 + 3 * 4);
-
-# functions are introduced with lambda or λ
-fib = lambda (n) if n < 2 then n else fib(n - 1) + fib(n - 2);
-
-println(fib(15));
-
-print-range = λ(a, b)             # λ is synonym to lambda
-                if a <= b then {  # then here is optional as you can see below
-                  print(a);
-                  if a + 1 <= b {
-                    print(", ");
-                    print-range(a + 1, b);
-                  } else println("");        # newline
-                };
-print-range(1, 5);
 `;
-
-const is = new InputStream(new Input(code));
-const tokens = [];
-while (!is.eof()) {
-    tokens.push(is.next());
+var FALSE = { type: "bool", value: false };
+function parse(input) {
+    var PRECEDENCE = {
+        "=": 1,
+        "||": 2,
+        "&&": 3,
+        "<": 7, ">": 7, "<=": 7, ">=": 7, "==": 7, "!=": 7,
+        "+": 10, "-": 10,
+        "*": 20, "/": 20, "%": 20,
+    };
+    return parse_toplevel();
+    function is_punc(ch) {
+        var tok = input.peek();
+        return tok && tok.type == "punc" && (!ch || tok.value == ch) && tok;
+    }
+    function is_kw(kw) {
+        var tok = input.peek();
+        return tok && tok.type == "kw" && (!kw || tok.value == kw) && tok;
+    }
+    function is_op(op) {
+        var tok = input.peek();
+        return tok && tok.type == "op" && (!op || tok.value == op) && tok;
+    }
+    function skip_punc(ch) {
+        if (is_punc(ch)) input.next();
+        else input.croak("Expecting punctuation: \"" + ch + "\"");
+    }
+    function skip_kw(kw) {
+        if (is_kw(kw)) input.next();
+        else input.croak("Expecting keyword: \"" + kw + "\"");
+    }
+    function skip_op(op) {
+        if (is_op(op)) input.next();
+        else input.croak("Expecting operator: \"" + op + "\"");
+    }
+    function unexpected() {
+        input.croak("Unexpected token: " + JSON.stringify(input.peek()));
+    }
+    function maybe_binary(left, my_prec) {
+        var tok = is_op();
+        if (tok) {
+            var his_prec = PRECEDENCE[tok.value];
+            if (his_prec > my_prec) {
+                input.next();
+                return maybe_binary({
+                    type     : tok.value == "=" ? "assign" : "binary",
+                    operator : tok.value,
+                    left     : left,
+                    right    : maybe_binary(parse_atom(), his_prec)
+                }, my_prec);
+            }
+        }
+        return left;
+    }
+    function delimited(start, stop, separator, parser) {
+        var a = [], first = true;
+        skip_punc(start);
+        while (!input.eof()) {
+            if (is_punc(stop)) break;
+            if (first) first = false; else skip_punc(separator);
+            if (is_punc(stop)) break;
+            a.push(parser());
+        }
+        skip_punc(stop);
+        return a;
+    }
+    function parse_call(func) {
+        console.log(func);
+        return {
+            type: "call",
+            func: func,
+            args: delimited("(", ")", ",", parse_expression),
+        };
+    }
+    function parse_varname() {
+        var name = input.next();
+        if (name.type != "var") input.croak("Expecting variable name");
+        return name.value;
+    }
+    function parse_if() {
+        skip_kw("if");
+        var cond = parse_expression();
+        if (!is_punc("{")) skip_kw("then");
+        var then = parse_expression();
+        var ret = {
+            type: "if",
+            cond: cond,
+            then: then,
+        };
+        if (is_kw("else")) {
+            input.next();
+            ret.else = parse_expression();
+        }
+        return ret;
+    }
+    function parse_lambda() {
+        return {
+            type: "lambda",
+            vars: delimited("(", ")", ",", parse_varname),
+            body: parse_expression()
+        };
+    }
+    function parse_bool() {
+        return {
+            type  : "bool",
+            value : input.next().value == "true"
+        };
+    }
+    function maybe_call(expr) {
+        expr = expr();
+        console.log('abcd', input.peek(), expr);
+        return is_punc("(") ? parse_call(expr) : expr;
+    }
+    function parse_atom() {
+        return maybe_call(function(){
+            if (is_punc("(")) {
+                console.log('(asdfasdfasfd');
+                input.next();
+                var exp = parse_expression();
+                skip_punc(")");
+                return exp;
+            }
+            if (is_punc("{")) {
+                console.log('{');
+                return parse_prog();
+            }
+            if (is_kw("if")) {
+                console.log('if');
+                return parse_if();
+            }
+            if (is_kw("true") || is_kw("false")) {
+                console.log('true');
+                return parse_bool();
+            }
+            if (is_kw("lambda") || is_kw("λ")) {
+                console.log('lambda');
+                input.next();
+                return parse_lambda();
+            }
+            var tok = input.next();
+            if (tok.type == "var" || tok.type == "num" || tok.type == "str") {
+                console.log('var');
+                return tok;
+            }
+            unexpected();
+        });
+    }
+    function parse_toplevel() {
+        var prog = [];
+        while (!input.eof()) {
+            const node = parse_expression();
+            prog.push(node);
+            if (!input.eof()) skip_punc(";");
+        }
+        return { type: "prog", prog: prog };
+    }
+    function parse_prog() {
+        var prog = delimited("{", "}", ";", parse_expression);
+        if (prog.length == 0) return FALSE;
+        if (prog.length == 1) return prog[0];
+        return { type: "prog", prog: prog };
+    }
+    function parse_expression() {
+        return maybe_call(function(){
+            return maybe_binary(parse_atom(), 0);
+        });
+    }
 }
-console.log(tokens);
+
+
+function myparse(input) {
+    return parse_toplevel();
+    function is_punc(ch) {
+        const tok = input.peek();
+        return tok && tok.type == "punc" && (!ch || tok.value == ch) && tok;
+    }
+    function skip_punc(ch) {
+        if (is_punc(ch)) {
+            input.next();
+        } else {
+            input.croak("Expecting punctuation: \"" + ch + "\"");
+        }
+    }
+    function parse_call(func) {
+        return {
+            type: "call",
+            func: func,
+            args: delimited("(", ")", ",", parse_expression),
+        };
+    }
+    function maybe_call(expr) {
+        expr = expr();
+        return is_punc("(") ? parse_call(expr) : expr;
+    }
+    function parse_toplevel() {
+        const prog = [];
+        while (!input.eof()) {
+            prog.push(parse_expression());
+            if (!input.eof()) skip_punc(';');
+        }
+        return {type: "prog", prog: prog};
+    }
+    function parse_expression() {
+        return maybe_call(() => {
+            return parse_atom();
+        });
+    }
+    function parse_atom() {
+        if (is_punc("{")) return parse_prog();
+        if (is_punc("(")) {
+            input.next();
+            const exp = parse_expression();
+            skip_punc(")");
+            return exp;
+        }
+        const tok = input.next();
+        if (tok.type == "var" || tok.type == "num" || tok.type == "str") {
+            return tok;
+        }
+        unexpected();
+    }
+    function unexpected() {
+        input.croak("Unexpected token: " + JSON.stringify(input.peek()));
+    }
+    function parse_prog() {
+        const prog = delimited("{", "}", ";", parse_expression);
+        if (prog.length == 0) return FALSE;
+        if (prog.length == 1) return prog[0];
+        return { type: "prog", prog: prog };
+    }
+}
+
+const ast = parse(new InputStream(new Input(code)));
+console.log(ast.prog);
+
